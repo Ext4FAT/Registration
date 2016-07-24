eb8029d3abe098e6389fb8bc65cc1c01a7670e21
_IDLER_ <zyy34472@gmail.com>
2016-07-24 22:30:02 +0800

    add para

--- ROBOT-GRASP/src/main.cpp
+++ ROBOT-GRASP/src/main.cpp
@@ -8,10 +8,25 @@ int main(int argc, char** argv)
 	double scale = argc > 1 ? atof(argv[1]) : 400.0;
 	string model_path = argc > 2 ? argv[2] : ".\\model\\bottle\\bottle.pcd";
 	string grasp_path = argc > 3 ? argv[3] : ".\\model\\bottle\\bottle-grasp-scaled.pcd";
-	//F200
-	MyRealsense robot(save_dir_path, 640, 480, 30);
+	
 	//Parameter
 	RegisterParameter para;
+	if (argc > 4)
+		para.leaf = atof(argv[4]);
+	if (argc > 5)
+		para.NumberOfSamples = atoi(argv[5]);
+	if (argc > 6)
+		para.CorrespondenceRandomness = atoi(argv[6]);
+	if (argc > 7)
+		para.SimilarityThreshold = atof(argv[7]);
+	if (argc > 8)
+		para.MaxCorrespondence = atof(argv[8]);
+	if (argc > 9)
+		para.InlierFraction = atof(argv[9]);
+	
+	//F200
+	MyRealsense robot(save_dir_path, 640, 480, 30);
+
 	robot.testRegistration(model_path, grasp_path, scale, para);
 	return 0;
 }
